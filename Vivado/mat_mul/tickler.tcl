acc34fff0b7c5634c713b4a26899ed140aff3a4a
Andrew Smith <atsmith3@illinois.edu>
2018-12-21 17:14:26 -0600

    Proceduralized the sysnthesis iteration

--- Vivado/mat_mul/tickler.tcl
+++ Vivado/mat_mul/tickler.tcl
@@ -5,35 +5,14 @@ set TOP_FUNCTION mat_mul_wrap
 set REPORT_END _csynth.rpt
 set REPORT $TOP_FUNCTION$REPORT_END
 
-
-#open file and gets the number of runs
-set infile [open "input.txt"]
-gets $infile line
-set runs $line
-
-set base sol
-
-#set project name and top function 
-open_project -reset $PROJECT_NAME 
-set_top $TOP_FUNCTION
-
-#Add files that need only be added once
-add_files mat_mul.h
-add_files -tb test_bench.c
-
-
-set outfile [open "report.out" w]
-
-
-for {set i 0} {$i < $runs } {incr i} {
-
+proc hls_iteration {solution_name file_name output_file report_file_name iter} {
   #name the current solution, set clock, set part
-  open_solution -reset $base$i
+  open_solution -reset $solution_name$iter
   set_part {xc7z020clg484-1} 
   create_clock -period 10 
 
   #get line in input and add it to project
-  gets $infile line
+  set line $file_name
   puts $line
   add_files $line
   
@@ -45,23 +24,52 @@ for {set i 0} {$i < $runs } {incr i} {
   #export_design 
   file delete $line
 
+  set report [open $report_file_name]
 
-  set infile_2 [open "$PROJECT_NAME\/$base$i\/syn\/report\/$REPORT"]
-
-  for {set j 0} {$j < 31 } {incr j} {
-    gets $infile_2 line_2
+  for {set j 0} {$j < 32 } {incr j} {
+    gets $report result_line
   }
 
-  gets $infile_2 full_line
-  puts $outfile "$base$i : $line"
-  puts $outfile "$full_line"
-  puts $outfile "\n" 
+  close $report
+
+  set res [regexp -all -inline {[0-9]+} $result_line]
+
+  set value [regexp -inline {[0-9]+} $res]
+
+#  gets $output_file result_line
+  puts $output_file "$solution_name$iter : $line"
+  puts $output_file "$res"
+  puts $output_file "$value"
+  puts $output_file "\n" 
+
+  return $value
+}
 
 
-
-  
-  
+#open file and gets the number of runs read in the files into an array
+set infile [open "files.txt"]
+set runs [gets $infile]
+array set file_arr {}
+for {set i 0} {$i < $runs} {incr i} {
+  gets $infile line
+  set file_arr($i) $line
 }
 close $infile
-close $infile_2
-close $outfile
+
+set base sol
+
+#set project name and top function 
+open_project -reset $PROJECT_NAME 
+set_top $TOP_FUNCTION
+
+#Add files that need only be added once
+add_files mat_mul.h
+add_files -tb test_bench.c
+
+set output_data [open "report.txt" w]
+
+for {set i 0} {$i < $runs } {incr i} {
+  set iter_result [hls_iteration $base $file_arr($i) $output_data "$PROJECT_NAME\/$base$i\/syn\/report\/$REPORT" $i]
+}
+
+close $output_data
