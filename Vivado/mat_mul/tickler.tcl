8ba0eeaf9a625267424e183b939cc9b3b25e658e
Andrew Smith <atsmith3@illinois.edu>
2018-12-21 19:21:18 -0600

    Almost working with the SA

--- Vivado/mat_mul/tickler.tcl
+++ Vivado/mat_mul/tickler.tcl
@@ -1,30 +1,44 @@
-set PROJECT_NAME tcl_1
-set TOP_FUNCTION mat_mul_wrap
+package require simulation::annealing 0.2
 
+global PROJECT_NAME
+set PROJECT_NAME tcl_1
+global TOP_FUNCTION
+set TOP_FUNCTION mat_mul_wrap
+global file_dir
+set file_dir mat_mul_files
 
 set REPORT_END _csynth.rpt
+global REPORT
 set REPORT $TOP_FUNCTION$REPORT_END
 
-proc hls_iteration {solution_name file_name output_file report_file_name iter} {
+proc hls_iteration {iter} {
+  #get info with callbacks
+  global solution_name
+  global array file_arr
+  global output_data
+  global PROJECT_NAME
+  global REPORT
+  global file_dir
+  set output_file $output_data
+  set f $file_arr($iter)
+
   #name the current solution, set clock, set part
   open_solution -reset $solution_name$iter
   set_part {xc7z020clg484-1} 
   create_clock -period 10 
 
   #get line in input and add it to project
-  set line $file_name
-  puts $line
-  add_files $line
-  
+  file copy "$file_dir/$f$" "."
+  add_files $f
   
   #run synthesis and delete file
   #csim_design
   csynth_design
   #cosim_design
   #export_design 
-  file delete $line
+  file delete $f
 
-  set report [open $report_file_name]
+  set report [open "$PROJECT_NAME\/$solution_name$iter\/syn\/report\/$REPORT"]
 
   for {set j 0} {$j < 32 } {incr j} {
     gets $report result_line
@@ -34,10 +48,10 @@ proc hls_iteration {solution_name file_name output_file report_file_name iter} {
 
   set res [regexp -all -inline {[0-9]+} $result_line]
 
-  set value [regexp -inline {[0-9]+} $res]
+  set value [expr [regexp -inline {[0-9]+} $res] + 0]
 
 #  gets $output_file result_line
-  puts $output_file "$solution_name$iter : $line"
+  puts $output_file "$solution_name$iter : $f"
   puts $output_file "$res"
   puts $output_file "$value"
   puts $output_file "\n" 
@@ -49,12 +63,15 @@ proc hls_iteration {solution_name file_name output_file report_file_name iter} {
 #open file and gets the number of runs read in the files into an array
 set infile [open "files.txt"]
 set runs [gets $infile]
+global array file_arr
 array set file_arr {}
 for {set i 0} {$i < $runs} {incr i} {
   gets $infile line
   set file_arr($i) $line
 }
 close $infile
+
+set files "files.txt"
 
 set base sol
 
@@ -66,10 +83,23 @@ set_top $TOP_FUNCTION
 add_files mat_mul.h
 add_files -tb test_bench.c
 
+global output_data
 set output_data [open "report.txt" w]
+set sa_report [open "sa_report.txt" w]
 
-for {set i 0} {$i < $runs } {incr i} {
-  set iter_result [hls_iteration $base $file_arr($i) $output_data "$PROJECT_NAME\/$base$i\/syn\/report\/$REPORT" $i]
-}
+global synth_result_fname
+set synth_result_fname "$PROJECT_NAME\/$base$i\/syn\/report\/$REPORT"
+
+global solution_name
+set solution_name "sol"
+
+puts [::simulation::annealing::findMinimum -verbose 1 -reportfile $sa_report -trials 3 -parameters {i 0 [$runs - 1]}  -code {set result [hls_iteration $i]}]
+
+#for {set i 0} {$i < $runs } {incr i} {
+  #set iter_result [hls_iteration $base $file_arr($i) $output_data "$PROJECT_NAME\/$base$i\/syn\/report\/$REPORT" $i]
+#}
 
 close $output_data
+close $sa_report
+
+exit
